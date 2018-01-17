57811493e36894e65b2967c437db157341b318f0
a-pecherskih <31979975+a-pecherskih@users.noreply.github.com>
2018-01-18 01:06:47 +0400

    Lab4_var24

--- Lab2_var24_Java/src/Airfield.java
+++ Lab2_var24_Java/src/Airfield.java
@@ -1,27 +1,51 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Airfield {
-	ClassArray<ITransport> airfield;
+	List<ClassArray<ITransport>> airfield;
 
     int countPlaces = 20;
     int placeSizeWidth = 210;
     int placeSizeHeight = 80;
+    int currentLevel;
 
-    public Airfield()
-    {
-        airfield = new ClassArray<ITransport>(countPlaces, null);
+    public int getCurrentLevel(){
+    	return currentLevel;
     }
+    
+    public Airfield(int countStages)
+    {
+        airfield = new ArrayList<ClassArray<ITransport>>();
+        for (int i = 0; i < countStages; i++){
+        	airfield.add(new ClassArray<ITransport>(countPlaces, null));
+        }
+        currentLevel = 0;
+    }
+    
+    public void LevelUp(){
+    	if(currentLevel + 1 < airfield.size()){
+    		currentLevel++;
+    	}
+    }
+    
+    public void LevelDown(){
+    	if(currentLevel > 0){
+    		currentLevel--;
+    	}
+    }
+
 
     public int PutPlaneInAirfield(ITransport plane)
     {
-        return airfield.add(airfield, plane);
+        return airfield.get(currentLevel).add(plane);
     }
 
     public ITransport GetPlaneFromAirfield(int ticket)
     {
-        return airfield.dec(airfield, ticket);
+        return airfield.get(currentLevel).dec(ticket);
     }
 
     public void Draw(Graphics g)
@@ -29,7 +53,7 @@ public class Airfield {
     	DrawMarking(g);
         for (int i = 0; i < countPlaces; i++)
         {
-        	ITransport plane = airfield.getObject(i);
+        	ITransport plane = airfield.get(currentLevel).getObject(i);
             if (plane != null)
             {
                 plane.setPosition(5 + i / 5 * placeSizeWidth + 5, i % 5 * placeSizeHeight - 15);
